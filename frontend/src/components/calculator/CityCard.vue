ea572ddf45e19e81d23ca1c435006d9c8b4b3a24
nikulinkv <nikulinkv@yandex.ru>
2023-05-29 11:32:27 +0500

    syncro

--- frontend/src/components/calculator/CityCard.vue
+++ frontend/src/components/calculator/CityCard.vue
@@ -1,32 +1,8 @@
 <template>
-  <div
-      class="filter-city"
-      v-for="city in calculation.cities"
-      :key="city"
-  >
+  <div class="filter-city">
     <h2>{{ city }}</h2>
-    <div class="filter-type" v-for="type in calculation.types" :key="type">
-      <h2>{{ type }}</h2>
-      <table>
-        <thead>
-        <tr>
-          <th>Место</th>
-          <th style="width: 200px">Дата начала</th>
-          <th style="width: 200px">Дата окончания</th>
-          <th>Цена</th>
-          <th>Цена</th>
-        </tr>
-        </thead>
-        <tbody>
-          <table-row v-for="row in rowArray"
-            :place="calculation.place"
-          ></table-row>
-        </tbody>
-      </table>
-      <button @click="rowArray.push({})">Добавить</button>
-    </div>
+    <slot></slot>
   </div>
-
 </template>
 
 <script>
@@ -36,8 +12,8 @@ import TableRow from "@/components/calculator/TableRow.vue";
 export default {
   components: {TableRow},
   props: {
-    calculation: {
-      type: Object
+    city: {
+      type: String
     }
   },
   data() {
@@ -45,23 +21,18 @@ export default {
       selected: '',
       rowArray: []
     }
-  },
-  computed: {
-  },
-  beforeMount() {
-    this.rowArray.push(this.calculation.place)
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .filter-city {
-  background: #fff;
+  background: #8e4adf;
   border-radius: 50px;
-  padding: 20px;
+  padding: 40px;
   text-align: center;
-  color: #8e4adf;
-  margin: 20px 0 20px;
+  color: #fff;
+  margin-bottom: 20px;
 }
 
 .filter-type {
---
+++ frontend/src/components/calculator/CityTable.vue
@@ -0,0 +1,73 @@
+<template>
+  <h2>{{ t }}</h2>
+  <table>
+    <thead>
+      <tr>
+        <th>Место</th>
+        <th style="width: 250px">Дата начала</th>
+        <th style="width: 250px">Дата окончания</th>
+        <th>Цена</th>
+        <th>Цена</th>
+      </tr>
+    </thead>
+    <tbody>
+      <table-row
+        v-for="row in tableRows"
+        :row="row"
+      ></table-row>
+
+    </tbody>
+  </table>
+  <button @click="tableRows.push({})">Добавить</button>
+</template>
+
+<script>
+import TableRow from "@/components/calculator/TableRow.vue";
+
+export default {
+  components: {TableRow},
+  props: {
+    t: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      tableRows: []
+    }
+  },
+  beforeMount() {
+    this.tableRows.push({})
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+  border-radius: 5px;
+
+  th,
+  td {
+    border: 2px #fff solid;
+    padding: 10px;
+    text-transform: uppercase;
+  }
+}
+
+h2 {
+  text-align: left;
+  color: #8be3a5;
+  margin: 20px 0;
+}
+
+button {
+  background: transparent;
+  border: none;
+  outline: none;
+  margin: 20px 0;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
